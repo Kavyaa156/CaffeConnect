7c9af96f606e9b5969501618ec969ffd05d006dd
Kavyaa More <morekavyaa06@gmail.com>
2025-12-17 16:32:01 +0530

    Updated the port for server.js

--- back-end/server.js
+++ back-end/server.js
@@ -11,7 +11,7 @@ const cafeRoutes = require('./routes/cafes');
 //we need mongoose
 const mongoose = require('mongoose');
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const app = express();
 
